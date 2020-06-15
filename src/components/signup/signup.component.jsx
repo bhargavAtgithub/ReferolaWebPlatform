9a78a36823ebf345e0c9d961f3701db58b0f7f3f
Bhargav <bhargav999reddy@gmail.com>
2020-06-15 17:09:44 +0530

    Added Components folder
    Contains all the components of the platform.

---
+++ src/components/signup/signup.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./signup.styles.scss";
+
+class Signup extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+}
+
+export default Signup;
